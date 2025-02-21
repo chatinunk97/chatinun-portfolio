82d1495b4d53167fd7b00d177e76405e9c4c4651
Chatinun <Chatinun.hobby@gmail.com>
2025-02-21 21:53:26 +0900

    add spotlight fix hydration error

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,11 @@ export default function Home() {
   return (
     // This is the whole web
     <main
-      className="relative bg-black flex 
+      className="relative bg-black-100 flex 
     justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5"
     >
       {/* This is the actual body of the web */}
-      <div className="max-w-7xl w-full bg-black">
+      <div className="max-w-7xl w-full">
         <Hero />
       </div>
     </main>
