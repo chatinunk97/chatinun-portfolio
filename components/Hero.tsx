021c596b11da40a47e5ae4a8703a205ee02af746
Chatinun <Chatinun.hobby@gmail.com>
2025-02-21 22:21:06 +0900

    add hero section text

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Spotlight } from "./ui/spotlight";
+import { TextGenerateEffect } from "./ui/text-generate-effect";
 
 const Hero = () => {
   return (
@@ -7,13 +8,13 @@ const Hero = () => {
       <div>
         <Spotlight
           className="top-40 -left-10 md:-lef5-32 md:-top-20 h-screen"
-          fill="white"
+          fill="black"
         />
         <Spotlight
           className="top-10 left-full h-[80vh] w-[50vw]"
-          fill="purple"
+          fill="#cf4b44"
         />
-        <Spotlight className="top-38 left-80  h-[80vh] w-[50vw]" fill="blue" />
+        <Spotlight className="top-38 left-80  h-[80vh] w-[50vw]" fill="#cf4b44" />
       </div>
       <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2]  flex items-center justify-center absolute top-0 left-0">
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
@@ -21,8 +22,15 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60w] flex flex-col items-center justify-center">
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic Web Magic with Next.js
+          Exploring, building, and sharing my journey through tech
           </h2>
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="Fueled by Innovation, Driven to learn, Built to Create"
+          />
+          <p className="text-center md:tracking-wide mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi, I'm Chatinun, a Trilingual Developer based in Japan
+          </p>
         </div>
       </div>
     </div>
